09de1afea4c41c70182cb04e1156da60e718c627
tfrenchnz <80009456+tfrenchnz@users.noreply.github.com>
2021-03-16 15:51:02 +1300

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,15 @@ import Layout from "../components/layout"
 import SEO from '~/components/seo'
 import ProductGrid from '~/components/ProductGrid'
 
-export default function Home() {
-  return <Layout>Hello</Layout>
-}
-
 const IndexPage = () => (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
-    <ProductGrid />
-    <Link to="/page-2/">Go to page 2</Link>
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
+      <ProductGrid />
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
   </>
 )
 
